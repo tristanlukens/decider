7a69ef032f00ab732389e4433a452f5349e57749
tristanlukens <71601628+tristanlukens@users.noreply.github.com>
2023-06-25 12:05:19 +0200

    created decider app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let val: string;
+	const handleClick = () => {
+		const rand = Math.random();
+		if (rand >= 0.5) {
+			val = 'Yes';
+		} else {
+			val = 'No';
+		}
+	};
+</script>
+
+<main class="h-screen flex justify-center items-center flex-col space-y-4">
+	<h1 class="font-bold text-3xl">Decider</h1>
+	<button
+		on:click={handleClick}
+		class="uppercase px-2 py-1 bg-green shadow rounded tracking-wider bg-sky-500 active:bg-sky-600 active:translate-y-0.5 hover:-translate-y-0.5 transition text-white font-bold"
+		>decide</button
+	>
+	<hr class="w-48" />
+	{#if val}
+		<p class="font-bold">{val}</p>
+	{:else}
+		<p class="text-neutral-400 italic">Click the button!</p>
+	{/if}
+</main>
